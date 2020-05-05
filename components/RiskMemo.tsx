ecaeb84182c088b7a8ddf521f6e11e0edfb79440
Chesley Brown <chesley.brown@icloud.com>
2020-05-05 13:17:08 -0300

    Fixed word in risk memo

--- components/RiskMemo.tsx
+++ components/RiskMemo.tsx
@@ -14,7 +14,7 @@ class RiskMemo extends React.Component<Props, State> {
                 <p>Risks should be explicitly stated and actions to address each categories with <i>R.O.A.M</i>:</p>
                 <ul>
                     <li><strong>Resolved:</strong> the risk is known and should be stated, but does not need action now</li>
-                    <li><strong>Owned:</strong> someone has taken ownership of addressing of the risk, but not yet resolved</li>
+                    <li><strong>Owned:</strong> someone has taken ownership of addressing the risk, but not yet resolved</li>
                     <li><strong>Accepted:</strong> the risk cannot be resolved, so it has to be understood and accepted for what it is</li>
                     <li><strong>Mitigated:</strong> a plan has been created to eradicate the risk</li>
                 </ul>
