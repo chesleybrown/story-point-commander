2a38dce096bdf381cd03fc4ad73a757e01635922
Chesley Brown <chesley.brown@icloud.com>
2020-07-17 09:49:42 -0300

    Added back exported Participant

--- services/Participants.ts
+++ services/Participants.ts
@@ -1,5 +1,13 @@
 import * as firebase from "firebase/app";
 
+export type Participant = {
+  name?: string;
+  solutionEffortOptionId?: string | null;
+  testingOptionId?: string | null;
+  riskOptionId?: string | null;
+  ready?: boolean;
+};
+
 type ParticipantClearUpdate = {
   solutionEffortOptionId: null;
   testingOptionId: null;
